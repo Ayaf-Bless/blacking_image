ec17aa86797d02bc006f0a50f35ae32abf53252f
Ayaf-Bless <blessambel1@gmail.com>
2022-07-24 21:15:47 +0300

    port change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const Prismic = require('@prismicio/client');
 const PrismicDOM = require('prismic-dom');
